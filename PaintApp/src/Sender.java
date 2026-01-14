715b9f8015ed1213a9d6ecf931defe25a8b457bb
giselleibarra <giselleibarra@users.noreply.github.com>
2026-01-13 17:22:30 -0800

    changing client to be sender

---
+++ PaintApp/src/Sender.java
@@ -0,0 +1,18 @@
+// import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Sender {
+    public static void send(String[] args) {
+        final String host = "localhost"; // Server IP addr
+        final int port = 6666;
+        try (Socket socket = new Socket(host, port); 
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+            String msg = "hello-"; // connect to paint app 
+            out.writeUTF(msg);
+            out.flush();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
