d4ee7a7299f5ffe78a4b9992ef1feb29825fa4b7
harinibaskar <hbaskar@calpoly.edu>
2026-01-13 16:59:22 -0800

    push

---
+++ MyClient.java
@@ -0,0 +1,17 @@
+import java.io.DataInputStream;
+import java.net.Socket;
+
+public class MyClient {
+    public static void main(String[] args) {
+        final String host = "localhost";
+        final int port = 6666;
+        try (Socket socket = new Socket(host, port);
+             DataInputStream in = new DataInputStream(socket.getInputStream())) {
+            String msg = in.readUTF();
+            System.out.println("Received: " + msg);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
