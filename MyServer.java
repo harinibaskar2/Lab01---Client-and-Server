ea9f1dba1366e12cfbca425739f31a9590a13530
Ethan Schnepp <152342752+EthanSchnepp@users.noreply.github.com>
2026-01-13 17:40:15 -0800

    Update MyServer.java

--- MyServer.java
+++ MyServer.java
@@ -1,27 +1,30 @@
-import java.io.DataOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+
+import java.net.*;
+
 
 public class MyServer {
+
     public static void main(String[] args) {
         final int port = 6666;
-        final int maxClients = 5;
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server listening on port " + port + " (will accept " + maxClients + " clients)...");
-            for (int i = 1; i <= maxClients; i++) {
-                System.out.println("Waiting for client " + i + "/" + maxClients + "...");
-                try (Socket socket = serverSocket.accept();
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
-                    String msg = "hello-" + i;
-                    out.writeUTF(msg);
-                    out.flush();
-                    System.out.println("Sent to " + socket.getRemoteSocketAddress() + ": " + msg);
-                }
+        System.out.println("SERVER: starting...");
+
+        try (ServerSocket ss = new ServerSocket(port);
+             Socket s = ss.accept();
+             DataInputStream dis = new DataInputStream(s.getInputStream())) {
+
+            System.out.println("SERVER: client connected: " + s.getRemoteSocketAddress());
+
+            while (true) {//Using DataInputStream.readUTF() to read the data from the client!
+                String msg = dis.readUTF();
+                System.out.println("SERVER: receiving " + msg);
             }
-            System.out.println("Done. Accepted " + maxClients + " clients. Server exiting.");
+
+        } catch (EOFException eof) {
+            System.out.println("SERVER: client disconnected. Server exiting.");
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("SERVER error: " + e);
         }
     }
 }
