fd4af78d2f87a7459f27eb32047f6f45c771ebae
Ethan Schnepp <152342752+EthanSchnepp@users.noreply.github.com>
2026-01-13 20:51:13 -0800

    Update MyServer.java
    UPDATED SERVER

--- MyServer.java
+++ MyServer.java
@@ -1,30 +1,29 @@
 import java.io.*;
-
 import java.net.*;
 
 
 public class MyServer {
-
     public static void main(String[] args) {
         final int port = 6666;
+        System.out.println("SERVER: starting on port " + port);
 
-        System.out.println("SERVER: starting...");
+        try (ServerSocket ss = new ServerSocket(port)) {
+            while (true) {
+                System.out.println("SERVER: waiting for connection...");
 
-        try (ServerSocket ss = new ServerSocket(port);
-             Socket s = ss.accept();
-             DataInputStream dis = new DataInputStream(s.getInputStream())) {
+                try (Socket s = ss.accept();
+                     DataInputStream dis = new DataInputStream(s.getInputStream())) {
 
-            System.out.println("SERVER: client connected: " + s.getRemoteSocketAddress());
+                    String msg = dis.readUTF();
+                    System.out.println("SERVER: receiving " + msg);
 
-            while (true) {//Using DataInputStream.readUTF() to read the data from the client!
-                String msg = dis.readUTF();
-                System.out.println("SERVER: receiving " + msg);
+                } catch (EOFException eof) {
+                    
+                }
             }
-
-        } catch (EOFException eof) {
-            System.out.println("SERVER: client disconnected. Server exiting.");
         } catch (Exception e) {
             System.out.println("SERVER error: " + e);
+            e.printStackTrace();
         }
     }
 }
